e1d5476ab1268a7fd8282f433f8b380d39485e46
unknown <simon.forwood@gmail.com>
2017-09-12 22:54:11 +1000

    support window resize

--- js/sample.js
+++ js/sample.js
@@ -4,5 +4,12 @@ window.onload = function () {
     
     sh = new stickyHelper(document.getElementById("stickyPane"), withString = true);
     sh.loadFromJson("sampleStickies.json");
-    sh.addSticky("And Another Note!", "This is a single created note!", 'color1', null)
+    sh.addSticky("And Another Note!", "This is a single created note!", 'color1', null);
 }
+
+window.addEventListener("resize", fooOnResize)
+
+function fooOnResize() {
+    sh.redraw();
+}
+
--- js/stickyWithString.js
+++ js/stickyWithString.js
@@ -12,43 +12,49 @@ class stickyHelper {
         this.saveInd = 0;
         this.hasStrings = hasStrings;
         this.stickiesElem = stickiesElem;
+        this.svg = null;
+        this.stickies = [];
 
-        if(this.hasStrings) {
-            var svgDiv = document.createElement('div');
-            svgDiv.setAttribute('class','stickWString');
-            this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            svgDiv.appendChild(this.svg);
-            this.stickiesElem.appendChild(svgDiv);
+        if(this.hasStrings)
+            this.createSvg();
+    }
 
-            var defs = document.createElementNS('http://www.w3.org/2000/svg','defs');
-            this.svg.append(defs);
-            var markerCircle = document.createElementNS('http://www.w3.org/2000/svg','marker');
-            markerCircle.setAttribute('id', 'markerCircle');
-            markerCircle.setAttribute('markerWidth', '8');
-            markerCircle.setAttribute('markerHeight','8');
-            markerCircle.setAttribute('refX','5');
-            markerCircle.setAttribute("refY", "5");
-            defs.append(markerCircle);
-            var circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
-            circle.setAttribute('cx', '5');
-            circle.setAttribute('cy', '5');
-            circle.setAttribute('r', '3');
-            circle.setAttribute('style','stroke: none; fill:#000000;');
-            markerCircle.append(circle);
+    createSvg() {
+        var svgDiv = document.createElement('div');
+        svgDiv.setAttribute('class','stickWString');
+        this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svgDiv.appendChild(this.svg);
+        
+        this.stickiesElem.insertBefore(svgDiv, this.stickiesElem.firstChild);
 
-            var markerArrow = document.createElementNS('http://www.w3.org/2000/svg','marker');
-            markerArrow.setAttribute('id', 'markerArrow');
-            markerArrow.setAttribute('markerWidth', '13');
-            markerArrow.setAttribute('markerHeight','13');
-            markerArrow.setAttribute('refX','2');
-            markerArrow.setAttribute("refY", "6");
-            markerArrow.setAttribute("orient", "auto");
-            defs.append(markerArrow);
-            var path = document.createElementNS('http://www.w3.org/2000/svg','path');
-            path.setAttribute('d', 'M2,2 L2,11 L10,6 L2,2');
-            path.setAttribute('style','fill: #000000;');
-            markerArrow.append(path);
-        }
+        var defs = document.createElementNS('http://www.w3.org/2000/svg','defs');
+        this.svg.append(defs);
+        var markerCircle = document.createElementNS('http://www.w3.org/2000/svg','marker');
+        markerCircle.setAttribute('id', 'markerCircle');
+        markerCircle.setAttribute('markerWidth', '8');
+        markerCircle.setAttribute('markerHeight','8');
+        markerCircle.setAttribute('refX','5');
+        markerCircle.setAttribute("refY", "5");
+        defs.append(markerCircle);
+        var circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
+        circle.setAttribute('cx', '5');
+        circle.setAttribute('cy', '5');
+        circle.setAttribute('r', '3');
+        circle.setAttribute('style','stroke: none; fill:#000000;');
+        markerCircle.append(circle);
+
+        var markerArrow = document.createElementNS('http://www.w3.org/2000/svg','marker');
+        markerArrow.setAttribute('id', 'markerArrow');
+        markerArrow.setAttribute('markerWidth', '13');
+        markerArrow.setAttribute('markerHeight','13');
+        markerArrow.setAttribute('refX','2');
+        markerArrow.setAttribute("refY", "6");
+        markerArrow.setAttribute("orient", "auto");
+        defs.append(markerArrow);
+        var path = document.createElementNS('http://www.w3.org/2000/svg','path');
+        path.setAttribute('d', 'M2,2 L2,11 L10,6 L2,2');
+        path.setAttribute('style','fill: #000000;');
+        markerArrow.append(path);
     }
 
     saveToJson(text, namePrefix) {
@@ -74,7 +80,7 @@ class stickyHelper {
         xobj.send(null);
     }
 
-    addSticky(title, desc, color, ulElem) {
+    addSticky(title, desc, color, element, ulElem) {
         if(!ulElem)
         {
             ulElem = this.stickiesElem.querySelector("ul.stickWString");
@@ -96,20 +102,28 @@ class stickyHelper {
         a.innerHTML="<h2>" + title + "</h2><p>" + desc + "</p>";
         li.appendChild(a);
 
+        this.stickies.push({
+            li: li,
+            element: element,
+            color:color
+        });
+
         if(this.hasStrings)
-            this.addStickyLine(li, color);
+            this.addStickyLine(li, element, color);
     }
 
-    addStickyLine(li, color) {
-        var line = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line.setAttribute('x1', li.offsetLeft + li.offsetWidth / 2);
-        line.setAttribute('y1', li.offsetTop + 10);
-        line.setAttribute('x2', li.offsetLeft + Math.random() * 200);
-        line.setAttribute('y2', Math.random() * 200 + 500);
-        line.setAttribute('stroke', stickyColors[color]);
-        line.setAttribute("stroke-width", 3);
-        line.setAttribute('marker-end', 'url(#markerArrow   )');
-        this.svg.append(line);
+    addStickyLine(li, element, color) {
+        if(element) {
+            var line = document.createElementNS('http://www.w3.org/2000/svg','line');
+            line.setAttribute('x1', li.offsetLeft + li.offsetWidth / 2);
+            line.setAttribute('y1', li.offsetTop + 10);            
+            line.setAttribute('x2', element.offsetLeft + element.offsetWidth / 2);
+            line.setAttribute('y2', element.offsetTop + element.offsetHeight / 2);
+            line.setAttribute('stroke', stickyColors[color]);
+            line.setAttribute("stroke-width", 3);
+            line.setAttribute('marker-end', 'url(#markerArrow)');
+            this.svg.append(line);
+        }
     }
 
     createStickys(stickyJson) {
@@ -124,7 +138,18 @@ class stickyHelper {
             for(var i=0; i<stickies.length; i++)
                 this.addSticky(stickies[i].title ? stickies[i].title : "",
                             stickies[i].desc ? stickies[i].desc : "",
-                            stickies[i].color ? stickies[i].color : null, ulElem);
+                            stickies[i].color,
+                            document.getElementById(stickies[i].element),
+                            ulElem);
         }
+    }
+
+    redraw() {
+        if(this.stickiesElem && this.stickiesElem.firstChild) {
+            this.stickiesElem.removeChild(this.stickiesElem.querySelector("div.stickWString"));
+        }
+        this.createSvg();
+        for(var i=0; i<this.stickies.length; i++)
+            this.addStickyLine(this.stickies[i].li, this.stickies[i].element, this.stickies[i].color);
     }
 }
